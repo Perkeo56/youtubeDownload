e20e5d4118f565e46c1d450b9ce486c101fc78a6
Perkeo56 <floriangruber6@gmail.com>
2022-10-25 11:09:53 +0200

    windows exe cmd file for easier use created

---
+++ create_windows_exe.cmd
@@ -0,0 +1,1 @@
+pyinstaller --noconfirm --onefile --windowed --icon "C:/Users/Flo/PycharmProjects/youtubeDownloadOnline/download-button.ico" --name "youtubeDownloader" --add-data "C:/Users/Flo/PycharmProjects/youtubeDownloadOnline/baum_800x600.png;."  "C:/Users/Flo/PycharmProjects/youtubeDownloadOnline/main.py"
